c8cdb2478398b0eae7d55626012aa0e88acd6b3d
4vermagaurav <4verma.gaurav@gmail.com>
2025-08-13 20:05:40 -0400

    completed pallindrome

--- src/main/java/com/ankur/misc/Pallindrome.java
+++ src/main/java/com/ankur/misc/Pallindrome.java
@@ -1,7 +1,25 @@
 package com.ankur.misc;
 
+import java.util.Arrays;
+
 public class Pallindrome {
     public static void main(String[] args) {
         System.out.println("Gaurav");
+        String str1 ="grv";
+        String str2 ="rvg";
+        char[] c1 = str1.toCharArray();
+        char[] c2 = str2.toCharArray();
+
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        if(Arrays.equals(c1,c2)){
+            System.out.println("this is pallindrome");
+        }else{
+            System.out.println("this is not pallindrome");
+        }
+
+
+
     }
 }
